26b0b3079b942e76c3e954d10d7e62f8499cd76d
Martin Marmsoler <martin.marmsoler@gmail.com>
2023-02-04 17:40:19 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+ flatpak-builder --force-clean --repo=dsremoteRepo build net.teuniz.dsremote.yml
---
+++ install.sh
@@ -0,0 +1,3 @@
+flatpak remove net.teuniz.dsremote
+flatpak remote-add dsremoteRepo dsremoteRepo --no-gpg-verify
+flatpak install dsremoteRepo net.teuniz.dsremote
